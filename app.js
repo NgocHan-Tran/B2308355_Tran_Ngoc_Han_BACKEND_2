a1693424e834dbc12f3573f6db1f0b18ad7d25c4
NgocHan-Tran <hann030904@gmail.com>
2026-04-03 10:20:27 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -1,39 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-const repairRouter = require("./app/routes/repair.route"); 
-const RepairService = require("./app/services/repair.service");
-const MongoDB = require('./app/utils/mongodb.util')
+const contactsRouter = require("./app/routes/contact.route");
 const ApiError = require("./app/api-error");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/repair", repairRouter);
-
-app.use("/api/repair", repairRouter);
-
-//handle 404 response 
-app.use((req, res, next) => {
-    // code o day se chay khi khong co route nao duoc dinh nghia
-    // khop voi yeu cau. Goi next() de chuyen sang middleware xu ly loi
-    return next(new ApiError(404, "Resource not found"));
-});
-
-//define error-handling middleware last, after other app.use() and routes calls 
-app.use((err, req, res, next) => {
-    // middleware xu ly loi tap trung
-    // Trong cac doan code xu ly o cac route, goi next(error) se chuyen ve middleware xu ly loi nay
-    return res.status(err.statusCode || 500).json({
-        message: err.message || "Internal Server Error",
-    });
-});
-app.use((error, req, res, next) => {
-    res.status(error.status || 500).send(error.message);
-});
+app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) => {
-    res.json({message: "Chào mừng bạn đến với ứng dụng đăng ký sửa chữa thiết bị"});
+res.json({message: "Welcome to contact book application."});
+
 });
 
 module.exports = app;
--- app/config/index.js
+++ app/config/index.js
@@ -3,7 +3,6 @@ const config = {
         port: process.env.PORT || 3000,
     },
     db: {
-        // Mình đã đổi 'contactbook' thành 'repair_management'
         uri: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/contactbook"
     }
 };
